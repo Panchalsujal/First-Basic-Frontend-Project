253749d4d03edbdeda6ffedc0e3393348a5dbca7
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-02-14 08:59:55 +0530

    Chnage 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./../src/index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Productcontext from "./ProductContext/Productcontext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Productcontext>
       <App />
     </Productcontext>
-  </BrowserRouter>,
+  </HashRouter>,
 );
